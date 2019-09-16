ba514c53172117bd109ad72a9c0b946b83fc5b1b
jtad009 <jtad009@gmail.com>
2019-09-16 12:13:05 +0100

    adds context to main app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { articles } from '../src/article';
 import Display from './Components/DisplayComponent';
-
+import {BlogContext} from './Store/Store'
 class App extends Component {
+    static contextType = BlogContext;
     constructor(){
         super();
         
@@ -14,30 +15,30 @@ class App extends Component {
         
     }
     componentDidMount(){
-        // const header = new Headers({
-        //     "Access-Control-Allow-Origin" : "*", 
-        //     "Access-Control-Allow-Credentials" : true ,
-        //     'Access-Control-Allow-Headers':'Origin, Content-Type, Authorization, X-Requested-With',
-        //     'Content-Type': 'application/json',
-        //     'Origin':'http://localhost:80/',
-        //     'Pragma':'no-cache',
-        //     'Cache-Control':'no-cache',
-        //     'Access-Control-Request-Method':'GET',
-        //     'Access-Control-Request-Headers':'access-control-allow-credentials,access-control-allow-headers,access-control-allow-origin,cache-control,content-type,pragma'
-        //    });
-        // fetch('http://localhost:80/api/v1/articles/all',{
-        //     crossDomain:true,
-        //     headers:header
+        const header = new Headers({
+            "Access-Control-Allow-Origin" : "*", 
+            "Access-Control-Allow-Credentials" : true ,
+            'Access-Control-Allow-Headers':'Origin, Content-Type, Authorization, X-Requested-With',
+            'Content-Type': 'application/json',
+            'Origin':'http://localhost:80/',
+            'Pragma':'no-cache',
+            'Cache-Control':'no-cache',
+            'Access-Control-Request-Method':'GET',
+            'Access-Control-Request-Headers':'access-control-allow-credentials,access-control-allow-headers,access-control-allow-origin,cache-control,content-type,pragma'
+           });
+        fetch('http://localhost:80/api/v1/articles/all',{
+            crossDomain:true,
+            headers:header
 
-        //   })
-        // .then(function(response){
-        //     console.log(response.headers);
-        //     return response.json();
-        // })
-        // .then(response=>console.log(response))
-        // .catch(function(error){
-        //     console.log("Error "+error);
-        // });
+          })
+        .then(function(response){
+            console.log(response.headers);
+            return response.json();
+        })
+        .then(response=>console.log(response))
+        .catch(function(error){
+            console.log("Error "+error);
+        });
         
     }
     onSearchChanged = (event) =>{
